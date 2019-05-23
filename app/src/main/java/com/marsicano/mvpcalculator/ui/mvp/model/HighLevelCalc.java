644ef8c6159ce1058747a55f07c8880301344f05
Javier Enrique Marsicano <javier.marsicano@simplex.software>
2019-05-23 16:46:12 -0300

    test unit test fix

--- app/src/main/java/com/marsicano/mvpcalculator/ui/mvp/model/HighLevelCalc.java
+++ app/src/main/java/com/marsicano/mvpcalculator/ui/mvp/model/HighLevelCalc.java
@@ -7,7 +7,7 @@ public class HighLevelCalc implements ICalcModel {
 
     @Override
     public double add(double op1, double op2) {
-        return op1 - op2;
+        return op1 + op2;
     }
 
     @Override
